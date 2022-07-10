2e2be42925747f70463f57d4af195a1f6da8fe0c
Ritu <tanujaru@gmail.com>
2022-07-10 13:24:29 -0400

    revisoncommit1

---
+++ revison_tanuja1/src/test/java/NewTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Tanuja\\installer\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.cnn.com/");
+  }
+}
